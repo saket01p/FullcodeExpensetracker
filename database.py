5414edb91d3b24d14d746f7fdf94017cc1226150
saket01p <saketpathak55@gmail.com>
2025-06-26 04:53:57 +0530

    updated the db name

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL="postgresql://postgres:saket1234@database-2.cxckkueqetdr.eu-west-1.rds.amazonaws.com:5432/ExpenseTrackerDatabase"
+DATABASE_URL="postgresql://postgres:saket1234@database-1.cxckkueqetdr.eu-west-1.rds.amazonaws.com:5432/ExpenseTrackerDatabase"
 
 
 engine=create_engine(DATABASE_URL)
