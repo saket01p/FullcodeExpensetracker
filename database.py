6858039d2a56f3e25b2c6c7710f57279ec71aa23
saket01p <saketpathak55@gmail.com>
2025-06-26 04:38:58 +0530

    added database rds config

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL="postgresql://postgres:Saket1234@localhost:5432/ExpenseTrackerDatabase"
+DATABASE_URL="postgresql://postgres:saket1234@database-2.cxckkueqetdr.eu-west-1.rds.amazonaws.com:5432/ExpenseTrackerDatabase"
+
+
 engine=create_engine(DATABASE_URL)
 sessionLocal=sessionmaker(bind=engine)
 
